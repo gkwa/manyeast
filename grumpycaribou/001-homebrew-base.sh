2511f4bc404a2070483607a6426707da12dc0ffb
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-25 11:55:09 -0800

    no need for partials, conditionals is enough

--- grumpycaribou/001-homebrew-base.sh
+++ grumpycaribou/001-homebrew-base.sh
@@ -54,7 +54,42 @@ EOF
 
 
 
+install_homebrew() {
+    local script=$(mktemp -p /tmp homebrew-XXXXXX)
+    chmod a+rx $script
+    cat >$script <<EOF
+timeout 30s curl --retry 9999 --connect-timeout 1 -sSf https://www.google.com >/dev/null
+export PATH=${HOMEBREW_PREFIX}/bin:\$PATH
 
+{{- if eq .Distro "alpine" }}
+# Alpine-specific installation
+LD_PRELOAD=/lib/libgcompat.so.0 NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+{{- else }}
+# RedHat/CentOS/Fedora installation
+NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+{{- end }}
+
+$HOMEBREW_SHELLENV_COMMAND
+
+
+if [ -f ~/.profile ]; then
+    if ! grep HOMEBREW_AUTO_UPDATE_SECS ~/.profile; then
+    echo 'export HOMEBREW_AUTO_UPDATE_SECS=\$((24*60*60))' >> ~/.profile
+    fi
+fi
+
+brew --version
+EOF
+    cd /home/linuxbrew
+    if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
+        sudo --login --user linuxbrew bash -e $script
+    else
+        echo "Homebrew is already installed. Skipping installation."
+    fi
+    rm -f $script
+}
 
 
 
