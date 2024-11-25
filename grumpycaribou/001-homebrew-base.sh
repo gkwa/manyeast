ab028a6a198cbe0f473a57b3f248983e03b64029
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-25 10:36:24 -0800

    musl

--- grumpycaribou/001-homebrew-base.sh
+++ grumpycaribou/001-homebrew-base.sh
@@ -5,7 +5,14 @@ set -e
 HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 HOMEBREW_SHELLENV_COMMAND='eval "$('${HOMEBREW_PREFIX}'/bin/brew shellenv)"'
 
+update_package_lists() {
+    if command -v apk &>/dev/null; then
+        apk update
+    fi
+}
+
 install_packages() {
+    update_package_lists
     if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
         apt-get update
         sudo apt-get install --assume-yes build-essential procps curl file git
@@ -31,25 +38,27 @@ EOF
     if ! id -u linuxbrew &>/dev/null; then
         sudo useradd --create-home linuxbrew --shell /bin/bash
     fi
+
     if ! test -f /etc/sudoers.d/linuxbrew; then
         echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
     fi
-
 }
 
 install_homebrew() {
     local script=$(mktemp -p /tmp homebrew-XXXXXX)
     chmod a+rx $script
+
     cat >$script <<EOF
 timeout 30s curl --retry 9999 --connect-timeout 1 -sSf https://www.google.com >/dev/null
 export PATH=${HOMEBREW_PREFIX}/bin:\$PATH
-NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+LD_PRELOAD=/lib/libgcompat.so.0 NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 $HOMEBREW_SHELLENV_COMMAND
 if ! grep HOMEBREW_AUTO_UPDATE_SECS ~/.profile; then
 echo 'export HOMEBREW_AUTO_UPDATE_SECS=\$((24*60*60))' >> ~/.profile
 fi
 brew --version
 EOF
+
     cd /home/linuxbrew
     if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
         sudo --login --user linuxbrew bash -e $script
