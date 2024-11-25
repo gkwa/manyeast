814ee7c76963a5028fae0ea0757f3061104037e6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-25 08:09:59 -0800

    debug

--- grumpycaribou/001-homebrew-base.sh
+++ grumpycaribou/001-homebrew-base.sh
@@ -6,33 +6,36 @@ HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 HOMEBREW_SHELLENV_COMMAND='eval "$('${HOMEBREW_PREFIX}'/bin/brew shellenv)"'
 
 install_packages() {
-    if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
-        apt-get update
-        sudo apt-get install --assume-yes build-essential procps curl file git
-    elif [ -f /etc/fedora-release ] || [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
-        sudo yum -y groupinstall 'Development Tools'
-        sudo yum -y install procps-ng curl file git
-    elif [ -f /etc/arch-release ]; then
-        sudo pacman -S base-devel procps-ng curl file git
-    else
-        echo "Unsupported distribution"
-        exit 1
-    fi
+   if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
+       apt-get update
+       sudo apt-get install --assume-yes build-essential procps curl file git
+   elif [ -f /etc/fedora-release ] || [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
+       sudo yum -y groupinstall 'Development Tools'
+       sudo yum -y install procps-ng curl file git
+       sudo rpm --import https://raw.githubusercontent.com/Homebrew/install/master/public_key.gpg
+   elif [ -f /etc/arch-release ]; then
+       sudo pacman -S base-devel procps-ng curl file git
+   elif [ -f /etc/alpine-release ]; then
+       sudo apk add build-base procps curl file git
+   else
+       echo "Unsupported distribution"
+       exit 1
+   fi
 }
 
 setup_linuxbrew_user() {
-    if ! id -u linuxbrew &>/dev/null; then
-        sudo useradd --create-home linuxbrew --shell /bin/bash
-    fi
-    if ! test -f /etc/sudoers.d/linuxbrew; then
-        echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
-    fi
+   if ! id -u linuxbrew &>/dev/null; then
+       sudo useradd --create-home linuxbrew --shell /bin/bash
+   fi
+   if ! test -f /etc/sudoers.d/linuxbrew; then
+       echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
+   fi
 }
 
 install_homebrew() {
-    local script=$(mktemp /tmp/homebrew-XXXXX.sh)
-    chmod a+rx $script
-    cat >$script <<EOF
+   local script=$(mktemp /tmp/homebrew-XXXXX.sh)
+   chmod a+rx $script
+   cat >$script <<EOF
 timeout 30s curl --retry 9999 --connect-timeout 1 -sSf https://www.google.com >/dev/null
 export PATH=${HOMEBREW_PREFIX}/bin:\$PATH
 NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -42,15 +45,16 @@ echo 'export HOMEBREW_AUTO_UPDATE_SECS=\$((24*60*60))' >> ~/.profile
 fi
 brew --version
 EOF
-    cd /home/linuxbrew
-    if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
-        sudo --login --user linuxbrew bash -e $script
-    else
-        echo "Homebrew is already installed. Skipping installation."
-    fi
-    rm -f $script
+   cd /home/linuxbrew
+   if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
+       sudo --login --user linuxbrew bash -e $script
+   else
+       echo "Homebrew is already installed. Skipping installation."
+   fi
+   rm -f $script
 }
 
 install_packages
 setup_linuxbrew_user
 install_homebrew
+
--- grumpycaribou/002-homebrew-configured.sh
+++ grumpycaribou/002-homebrew-configured.sh
@@ -6,29 +6,29 @@ HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 HOMEBREW_SHELLENV_COMMAND='eval "$('${HOMEBREW_PREFIX}'/bin/brew shellenv)"'
 
 add_homebrew_shellenv() {
-    local file="$1"
-    if ! grep -q "Setup Homebrew if it exists" "$file"; then
-        echo "
+   local file="$1"
+   if ! grep -q "Setup Homebrew if it exists" "$file"; then
+       echo "
 # Setup Homebrew if it exists
 if [ -d \"$HOMEBREW_PREFIX\" ]; then
-  export PATH=\"/usr/local/bin:\$PATH\"
-  $HOMEBREW_SHELLENV_COMMAND
+ export PATH=\"/usr/local/bin:\$PATH\"
+ $HOMEBREW_SHELLENV_COMMAND
 else
-  echo \"Homebrew is not installed in the expected location.\" >&2
+ echo \"Homebrew is not installed in the expected location.\" >&2
 fi" >>"$file"
-    fi
+   fi
 }
 
 add_root_brew_wrapper() {
-    local file="$1"
-    if [ "$EUID" -eq 0 ] && ! grep -q "Function to run brew as linuxbrew user" "$file"; then
-        echo '
+   local file="$1"
+   if [ "$EUID" -eq 0 ] && ! grep -q "Function to run brew as linuxbrew user" "$file"; then
+       echo '
 # Function to run brew as linuxbrew user
 brew() {
-   sudo --user linuxbrew --login brew "$@"
+  sudo --user linuxbrew --login brew "$@"
 }
 ' >>"$file"
-    fi
+   fi
 }
 
 add_homebrew_shellenv ~/.bashrc
