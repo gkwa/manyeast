23093fd944c22d21f07401064ec7872e1a1a08ff
Taylor Monacelli <taylormonacelli@gmail.com>
2025-02-22 09:32:22 -0800

    return type of the listener needs to be true to indicate we want to handle the message asynchronously

--- regalraven/src/background/service-worker.ts
+++ regalraven/src/background/service-worker.ts
@@ -1,12 +1,24 @@
 import browser from "webextension-polyfill"
 
-// Initialize message listener
-browser.runtime.onMessage.addListener((message, sender) => {
-  if (message.type === "contentScriptLoaded") {
+// Define the message type
+interface ContentScriptMessage {
+  type: "contentScriptLoaded"
+}
+
+// Initialize message listener with proper typing
+browser.runtime.onMessage.addListener((message: unknown, sender, sendResponse) => {
+  // Type guard to verify the message structure
+  const isContentScriptMessage = (msg: unknown): msg is ContentScriptMessage => {
+    return typeof msg === 'object' && msg !== null && 'type' in msg &&
+           msg.type === 'contentScriptLoaded'
+  }
+
+  if (isContentScriptMessage(message)) {
     console.log("Content script loaded in tab:", sender.tab?.id)
     return Promise.resolve({ received: true })
   }
-  return false
+
+  return true // Return true to indicate we'll send a response asynchronously
 })
 
 // Log when the service worker is initialized
