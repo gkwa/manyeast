02d58e6261f8fe383675425a9a1d56c9e7e7607c
Taylor Monacelli <taylormonacelli@gmail.com>
2025-05-16 15:03:30 -0700

    prompts

--- littlelark/generate_txtar.sh
+++ littlelark/generate_txtar.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
 {{/* Get repository name using path API and replaceAll for .git */ -}}
 {{ $repoBase := base .RepoURL -}}
 {{ $repoName := replaceAll ".git" "" $repoBase -}}
 {{ $clonedDir := printf "%s/%s" .BaseDir $repoName -}}
 {{ $subsetDir := printf "%s_subset" $clonedDir -}}
-{{ $manifest := printf "%s.txt" $clonedDir }}
-{{ $filterManifest := printf "%s_filter.txt" $clonedDir }}
-{{ $mimeTypesFile := printf "%s_mime.txt" $clonedDir }}
+{{ $manifest := printf "%s.txt" $clonedDir -}}
+{{ $filterManifest := printf "%s_filter.txt" $clonedDir -}}
+{{ $mimeTypesFile := printf "%s_mime.txt" $clonedDir -}}
+{{ $txtarFile := printf "%s/%s_subset.txtar" .OutputFolder $repoName -}}
 
 test ! -d {{ $clonedDir }} && git clone {{ .RepoURL }} {{ $clonedDir }}
 
@@ -23,15 +22,15 @@ else
     rg --files . -0 | xargs -0 file --mime-type >{{ $manifest }}
 fi
 
-test ! -f {{ $mimeTypesFile }} &&
-    cat {{ $manifest }} | cut -d: -f2 | perl -p -e 's#^ *##' | sort -u > {{ $mimeTypesFile }}
+test ! -f {{ $mimeTypesFile }} && cat {{ $manifest }} | cut -d: -f2 |
+    perl -p -e 's#^ *##' | sort -u > {{ $mimeTypesFile }}
 
 grep --invert-match --file {{ $mimeTypesFile }} {{ $manifest }} |
     cut --delimiter : --fields 1 |
     cpio --insecure --pass-through --make-directories {{ $subsetDir }}
 
-txtar-c {{ $subsetDir }} >$SCRIPT_DIR/{{ $repoName }}_subset.txtar
-du -shc $SCRIPT_DIR/{{ $repoName }}_subset.txtar
+txtar-c {{ $subsetDir }} >{{ $txtarFile }}
+du -shc {{ $txtarFile }}
 
 find {{ .BaseDir }} -maxdepth 1
 
